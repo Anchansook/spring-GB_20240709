5ab3b391cc08a0e178bd51d6d662c2e83d4bbe30
ckstnr99 <ckstnr99@naver.com>
2024-08-22 14:51:45 +0900

    2024.08.22. 14:51

--- src/main/java/com/acs/springbasic/controller/RequestDataController.java
+++ src/main/java/com/acs/springbasic/controller/RequestDataController.java
@@ -2,6 +2,8 @@ package com.acs.springbasic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/request-data")
 public class RequestDataController {
     
-    // @RequestParam() : 
+    //# @RequestParam() : 
     // - GET, DELETE 처럼 Request Body가 존재하지 않고, 
     //   URL로 데이터를 전송하는 메서드에서 Query String 방식으로 
     //   데이터를 가져오기 위해 사용하는 어노테이션
@@ -27,7 +29,7 @@ public class RequestDataController {
         return "이름 : " + name + " 나이 : " + age;
     };
 
-    // @PathVariable() :
+    //# @PathVariable() :
     // 모든 HTTP 메서드에서 URL의 특정 패턴에 따라 데이터를 추출 (URL에서 추출하는 것임)
     // GET http://localhost:4000/request-data/path-variable/받고싶어값~/하나더
     // 기능을 기준으로 이름 설정 (데이터베이스 기준 X)
@@ -66,5 +68,49 @@ public class RequestDataController {
     ) {
         return "another2";
     };
+
+    //# @RequestBody() :
+    // - POST, PUT, PATCH 처럼 Request Body로 데이터를 전송하는 메서드에서 데이터를 읽기 위한 방법
+    //& 가끔 문법이 맞는데 에러 뜰 때 전체 복사 후 다시 붙여넣고, 리 컴파일
+    @PostMapping("/request-body")
+    public String requestBody(
+        // @RequestBody String requestBody
+        @RequestBody SampleDto requestBody
+    ) {
+        return "Request Body data : " + requestBody.getName() + ", " + requestBody.getAge();
+        // 그냥은 주소 참조값만 나옴, 그래서 requestBody.getName(), requestBody.getAge() 이렇게 확인!
+    };
+
+}
+//# DTO(Data Transfer Object) :
+// - 데이터를 서로 다른 계층간에 전송하기 위한 객체
+// - 캡슐화가 되어 있음, 비즈니스 로직은 포함하지 않고, 'private 필드'와 '생성자', 'getter', 'setter'만 존재
+// - JSON 형식으로 받을 때 사용
+class SampleDto {
+    
+    private String name;
+    private int age;
+
+    public SampleDto() {}
+    public SampleDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
 
 }
