4b646697a8327801895852640cec5c478bc455f6
ckstnr99 <ckstnr99@naver.com>
2024-08-22 15:52:10 +0900

    2024.08.22. 15:52

--- src/main/java/com/acs/springbasic/SpringbasicApplication.java
+++ src/main/java/com/acs/springbasic/SpringbasicApplication.java
@@ -3,11 +3,28 @@ package com.acs.springbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.acs.springbasic.dto.Lombok;
+
 @SpringBootApplication
 public class SpringbasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbasicApplication.class, args);
+	}
+
+	// Lombok.java
+	void lombokExample() {
+
+		Lombok lombok = new Lombok("a", "b", "c", false, false);
+		lombok.getField1();
+		lombok.setField3(null);
+
+		// lombok = new Lombok(); => @NoArgsConstructor 사용했을 때
+		lombok = new Lombok("d", "e");
+
+		lombok.isField4();
+		lombok.getField5();
+
 	}
 
 }
