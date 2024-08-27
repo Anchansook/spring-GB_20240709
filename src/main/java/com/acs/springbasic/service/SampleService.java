72ce725eded3a7747574ef4be8ee390f9bf72657
ckstnr99 <ckstnr99@naver.com>
2024-08-27 16:01:21 +0900

    2024.08.27. 16:01

---
+++ src/main/java/com/acs/springbasic/service/SampleService.java
@@ -0,0 +1,11 @@
+package com.acs.springbasic.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.acs.springbasic.dto.PostSample1RequestDto;
+
+public interface SampleService {
+    
+    ResponseEntity<String> postSample1 (PostSample1RequestDto dto);
+
+}
