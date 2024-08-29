195d1d03a4ed34495d6051d9cc7a6f18cf4e1e91
ckstnr99 <ckstnr99@naver.com>
2024-08-29 16:51:02 +0900

    2024.08.29. 16:50

--- src/main/java/com/acs/springbasic/service/implement/SampleServiceImplement.java
+++ src/main/java/com/acs/springbasic/service/implement/SampleServiceImplement.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.acs.springbasic.dto.PostSample1RequestDto;
 import com.acs.springbasic.entity.SampleTable1Entity;
 import com.acs.springbasic.entity.SampleUserEntity;
+import com.acs.springbasic.provider.JwtProvider;
 import com.acs.springbasic.repository.SampleTable1Repository;
 import com.acs.springbasic.repository.SampleUserRepository;
 import com.acs.springbasic.service.SampleService;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SampleServiceImplement implements SampleService {
 
+    private final JwtProvider jwtProvider;
     private final SampleUserRepository sampleUserRepository;
     private final SampleTable1Repository sampleTable1Repository;
 
@@ -73,6 +75,12 @@ public class SampleServiceImplement implements SampleService {
         List<SampleUserEntity> sampleUserEntities = sampleUserRepository.getNativeSql("홍길동", "부산광역시");
 
         return ResponseEntity.status(HttpStatus.OK).body(sampleUserEntities.toString());
+    }
+
+    @Override
+    public String getJwt(String name) {
+        String jwt = jwtProvider.create(name);
+        return jwt;
     }
     
 }
